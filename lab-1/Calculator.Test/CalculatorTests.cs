4c24e52f138084b73fa20e03d55a22b25b47b792
Thomas Elbo <thomas.m.elbo@gmail.com>
2023-10-24 13:23:30 +0200

    Removed initial test case from Lab-1

--- lab-1/Calculator.Test/CalculatorTests.cs
+++ lab-1/Calculator.Test/CalculatorTests.cs
@@ -10,13 +10,6 @@ namespace Calculator.Test
         {
             calculator = new Calculator();
         }
-
-        [Test]
-        [TestCase(1, 1, 1)]    // Test case with parameters (1 * 1 = 1)
-        public void ProductTest(int a, int b, int expectedResult)
-        {
-            int result = calculator.Product(a, b);
-            Assert.That(expectedResult, Is.EqualTo(result));
-        }
+        
     }
 }
